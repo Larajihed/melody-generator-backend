3d3e22567cc38e998f4b159e3ce188ac4886729a
Jihed Larayedh <larajihade@gmail.com>
2023-04-04 03:41:33 +0200

    added vercel

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const Melody = require('./models/melody');
 
 const app = express();
 //app.use(cors({credentials: true, origin: 'http://localhost:3000'}));
-app.use(cors({credentials: true, origin: 'https://melody-generator-backend.vercel.app/'}));
+app.use(cors({credentials: true, origin: 'https://dev.dj19csamz5x4m.amplifyapp.com/'}));
 const port = process.env.PORT || 4033;
 
 const authenticationRouter = require('./routes/AuthenticationController');
