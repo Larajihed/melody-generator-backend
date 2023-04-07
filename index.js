dc99ec74935cdc58e0eac4416117199a76104ec6
Jihed Larayedh <larajihade@gmail.com>
2023-04-07 02:59:07 +0200

    origin/

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const Melody = require('./models/melody');
 
 const app = express();
 //app.use(cors({credentials: true, origin: 'http://localhost:3000'}));
-app.use(cors({credentials: true, origin: 'https://melody-generator-front-orcin.vercel.app/'}));
+app.use(cors({credentials: true, origin: 'https://melody-generator-front-orcin.vercel.app'}));
 
 const port = process.env.PORT || 4033;
 
