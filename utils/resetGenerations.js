003e5fa1072cc58f2364b09674c693fb12ba4d5c
Jihed Larayedh <larajihade@gmail.com>
2023-04-08 04:16:56 +0200

    cron2

--- utils/resetGenerations.js
+++ utils/resetGenerations.js
@@ -3,16 +3,10 @@ const { User } = require('../models/user'); // Adjust the path to your User mode
 export default async function handler(request, response) {
     try{
         await User.updateMany({ generations: { $lt: 5 } }, { $set: { generations: 5 } });
-        res.status(200).send('Generations reset to 5 for all users');
-        response.status(200).json({
-          body: request.body,
-          query: request.query,
-          cookies: request.cookies,
-        });
-
     }catch(error){
         console.error(error)
         res.status(500).send('Error resetting generations');
-
     }
+    res.status(200).send('Generations reset to 5 for all users');
+
   }
