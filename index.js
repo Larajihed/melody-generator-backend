725fb0bcca4d85f5aa9c7946e9ed9d44cabc8eca
Jihed Larayedh <larajihade@gmail.com>
2023-04-08 06:04:18 +0200

    add User model

--- index.js
+++ index.js
@@ -49,10 +49,10 @@ mongoose.connect(process.env.MONGODB_URI, {
   app.get('/api/resetGenerations', async (req, res) => {
     try {
       await User.updateMany({}, { $set: { generations: 5 } });
-      response.status(200).send('Generations reset to 5 for all users');
+      res.status(200).send('Generations reset to 5 for all users');
     } catch (error) {
       console.error(error);
-      response.status(500).send('Error resetting generations');
+      res.status(500).send('Error resetting generations');
     }
   });
   
