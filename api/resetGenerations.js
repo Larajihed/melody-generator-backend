bac16d6f4fac41a4961e1b8717c7a5c2ed614c2f
Jihed Larayedh <larajihade@gmail.com>
2023-04-08 05:55:41 +0200

    main

--- api/resetGenerations.js
+++ api/resetGenerations.js
@@ -3,10 +3,10 @@ const { User } = require('../models/user');
 async function resetGenerations(request, response) {
   try {
     await User.updateMany({}, { $set: { generations: 5 } });
-    response.send('Generations reset to 5 for all users');
+    response.status(200).send('Generations reset to 5 for all users');
   } catch (error) {
     console.error(error);
-    response.send('Error resetting generations');
+    response.status(500).send('Error resetting generations');
   }
 }
 
